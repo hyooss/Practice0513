a9003d9f45eb039b3be5cc7e806a8691801eb7db
hyeonee <hyoosscode@gmail.com>
2025-05-13 10:12:04 +0900

    1

---
+++ linear_stack.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <malloc.h>
+
+#define MAX_SIZE 5
+
+int main(void) {
+    //배열을 사용한 스택
+    int data[MAX_SIZE]; // 스택으로 사용할 배열
+    int top; //스택에서 맨 위에 있는 데이터 위치
+    int index;
+    int popItem;
+
+    // 스택의 초기화(비어있음)
+    top = -1;
+
+    //push 10
+    top++; //top = top + 1
+    data[top]=10;
+
+    //push 20
+    top++;
+    data[top]=20;
+
+    //push 30
+    top++;
+    data[top]=30;
+
+    //pop
+    popItem = data[top];
+    top--;
+    printf("Pop Item: %d\n,popItem");
+
+    //display
+    printf("Linear Stack= [");
+    for (index = 0; index <= top; index++) {
+        printf("%d, ", data[index]);
+        if (index!=top) {
+            printf(" | ");
+        }
+    }
+    printf("]");
+
+    return 0;
+}
